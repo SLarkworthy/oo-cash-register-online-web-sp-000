2c30bd20fe5e9693bfa644bff08b99c7e43580ef
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-14 19:52:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
-
+class CashRegister
+  attr_accessor :total, :employee_discount
+  
+  def initialize(discount = nil)
+    @total = 0
+    self.discount
+  end
+  
+  def discount
+    @discount = discount
+  end
+  
+  
+  
+end
