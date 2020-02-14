8b8e5d60d513863a35f05f0cf556e130afb34a9f
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-14 19:59:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   
   def apply_discount
     @total = @total - (@total * discount * 0.01)
-    "After the discount, the total comes to $#{@total.truncate}."
+    "After the discount, the total comes to $#{@total.truncate()}."
   end
   
   
